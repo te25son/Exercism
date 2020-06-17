ee2d4248345885a1c3a4c84b349ccaf62f1b90bb
te25son <te25son@gmail.com>
2020-06-17 21:17:33 +0200

    Add isogram project

---
+++ csharp/isogram/Isogram.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class Isogram
+{
+    public static bool IsIsogram(string word)
+    {
+        var characterSet = new List<char>();
+
+        foreach (var character in word.ToLower())
+        {
+            if (!char.IsLetter(character) || (char.IsLetter(character) && !characterSet.Contains(character)))
+            {
+                characterSet.Add(character);
+            }
+        }
+
+        return characterSet.Count.Equals(word.Length);
+    }
+}
