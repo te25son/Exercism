f924e514e58aceb660169faec463770443329f5a
te25son <te25son@gmail.com>
2020-06-17 17:27:09 +0200

    Initial commit

---
+++ csharp/pangram/Pangram.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class Pangram
+{
+    public static bool IsPangram(string input)
+    {
+        var characterSet = new HashSet<char>();
+
+        foreach (var character in input)
+        {
+            if (char.IsLetter(character))
+            {
+                characterSet.Add(char.ToLower(character));
+            }
+        }
+
+        return characterSet.Count.Equals(26);
+    }
+}
