b87527848f61d65fb9acdc2e0a7ec07a6783301f
te25son <te25son@gmail.com>
2020-06-18 07:41:01 +0200

    WIP

---
+++ csharp/acronym/Acronym.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+public static class Acronym
+{
+    public static string Abbreviate(string phrase)
+    {
+        var splitPhrase = phrase.Split(" ").ToList();
+        var abbreviation = string.Empty;
+
+        foreach (var word in splitPhrase)
+        {
+            if (char.IsLetter(word[0]))
+            {
+                abbreviation += word[0];
+            }
+        }
+
+        return abbreviation;
+    }
+}
